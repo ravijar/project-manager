e6d946b3006b887d021c715385bd8dff1de1ae8a
RavijaRandinu <r.randinu@gtngroup.com>
2025-08-13 06:17:00 +0530

    added private chat creation logic for assignments

--- src/services/assignmentService.js
+++ src/services/assignmentService.js
@@ -4,14 +4,25 @@ import {addAssignmentField, getAllAssignmentFields} from "../firebase/firestore/
 import {v4 as uuidv4} from "uuid";
 import {addParticipant, createNewGroupChat} from "./chatService.js";
 import {Timestamp} from "firebase/firestore";
+import {createChat} from "../firebase/firestore/chatsCollection.js";
 
 export const generateAssignmentId = () => {
     return "assignment_" + uuidv4()
 };
 
+export const generateChatId = (assignmentId, role) => {
+    return [assignmentId, role].join("_");
+};
+
 export const addNewAssignment = async (assignmentId, assignmentData, user) => {
-    const chatId = await createNewGroupChat(assignmentData?.name, [user], user, true, assignmentId);
-    await addAssignment(assignmentId, {...assignmentData, student: user.id, chatId: chatId});
+    const groupChatId = await createNewGroupChat(assignmentData?.name, [user], user, true, assignmentId);
+    const chatIds = {
+        group: groupChatId,
+        tutor: null,
+        student: null,
+    };
+
+    await addAssignment(assignmentId, {...assignmentData, student: user.id, chatIds});
     await addAssignmentToUser(user.id, assignmentId);
 };
 
@@ -24,17 +35,33 @@ export const fetchAssignmentById = async (assignmentId) => {
     }
 };
 
+export const createNewPrivateChat = async (assignment, role) => {
+    const chatId = generateChatId(assignment.id, role);
+    const fieldPath = `chatIds.${role}`;
+
+    await createChat(chatId, [assignment.admin, assignment[role]], null, assignment.admin, true, assignment.id);
+
+    await updateAssignment(assignment.id, { [fieldPath]: chatId });
+
+    return chatId;
+};
+
 export const assignAdminToAssignment = async (assignmentId, adminUserId) => {
     try {
         const assignment = await fetchAssignmentById(assignmentId);
-        const chatId = assignment.chatId;
+        const groupChatId = assignment.chatIds?.group;
 
-        await addParticipant(adminUserId, chatId);
+        groupChatId && await addParticipant(adminUserId, groupChatId);
+
         await addAssignmentToUser(adminUserId, assignmentId);
+
         await updateAssignment(assignmentId, {
             admin: adminUserId,
             subStatus: "admin_assigned"
         });
+
+        assignment.student && await createNewPrivateChat({ ...assignment, admin: adminUserId }, "student");
+
     } catch (error) {
         console.error("Failed to assign admin to assignment:", error);
         throw error;
@@ -44,15 +71,20 @@ export const assignAdminToAssignment = async (assignmentId, adminUserId) => {
 export const assignTutorToAssignment = async (assignmentId, tutorUserId) => {
     try {
         const assignment = await fetchAssignmentById(assignmentId);
-        const chatId = assignment.chatId;
+        const groupChatId = assignment.chatIds?.group;
 
-        await addParticipant(tutorUserId, chatId);
+        groupChatId && await addParticipant(tutorUserId, groupChatId);
+
         await addAssignmentToUser(tutorUserId, assignmentId);
+
         await updateAssignment(assignmentId, {
             tutor: tutorUserId,
             tutorStartedOn: Timestamp.fromDate(new Date()),
             subStatus: "tutor_assigned"
         });
+
+        assignment.admin && await createNewPrivateChat({ ...assignment, tutor: tutorUserId }, "tutor");
+
     } catch (error) {
         console.error("Failed to assign tutor to assignment:", error);
         throw error;
