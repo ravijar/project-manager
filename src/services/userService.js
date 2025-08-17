ce364f2082d47ce582c927e07add72ce862f69bf
RavijaRandinu <r.randinu@gtngroup.com>
2025-08-17 21:39:53 +0530

    synced workspace

--- src/services/userService.js
+++ src/services/userService.js
@@ -49,3 +49,16 @@ export const getOtherUserFromChatId = async (chatId, currentUserId) => {
         return null;
     }
 };
+
+export const getOtherUserFromParticipants = async (participants, currentUserId) => {
+    try {
+        const otherUserId = participants.find(id => id !== currentUserId);
+
+        if (!otherUserId) return null;
+
+        return await readUser(otherUserId);
+    } catch (error) {
+        console.error("Error fetching other user from chat ID:", error);
+        return null;
+    }
+};
